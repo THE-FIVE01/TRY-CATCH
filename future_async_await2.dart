45877fa6519c31d6bebd681764a5e964f44949a7
THE-FIVE01 <tialaonahousse@gmailcom>
2021-02-10 15:58:18 +0000

    Update

--- future_async_await2.dart
+++ future_async_await2.dart
@@ -1,14 +1,23 @@
 // Toutes les fonctions déclarées ci-dessous s'exécutent sur le fil de l'interface utilisateur principale.
 import 'dart:async';
 
-main() {}
+main() {
+  print("Main program: Starts");
+  printFileContent();
+  print("Main program: Ends");
+}
 
-printFileContent() {}
+printFileContent() {
+  Future<String> fileContent = downLoadAFile();
+  print("The content of the file is --> $fileContent");
+}
 
 // Pour télécharger un fichier [opération de longue durée factice]
-downLoadAFile() {
+Future<String> downLoadAFile() {
   Future<String> result = Future.delayed(Duration(seconds: 6), () {
     // 1sec, 2sec,.... 6sec
     return "My secret file content";
   });
+
+  return result;
 }
